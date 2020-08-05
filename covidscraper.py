b65881d1a1fd33c4451dfa1fd84012b5cbacc0e6
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-08-05 19:33:55 +0800

    getting covid-19 stats from google
    first attempt at trying to web scrape from google using beautifulsoup. program that tell's you the current day's covid-19 stats for any country you search for.

---
+++ covidscraper.py
@@ -0,0 +1,39 @@
+from requests_html import HTMLSession
+
+from bs4 import BeautifulSoup
+
+
+session = HTMLSession()
+
+running = True
+while running:
+    ask = input('which country\'s covid-19 stats for today would you like to know more about?\n')
+    askcovid = ask + ' covid19 cases'
+    par = {'q': askcovid}
+    r = session.get('https://www.google.com/search', params=par)
+
+    soooup = BeautifulSoup(r.text, 'html.parser')
+    stats = soooup.findAll('td', {'jsname': 'VBiLTe'})
+
+    index = 0
+    for i in stats:
+        caption1 = i.find('div').text
+        stats1 = i.findAll('div')[1]
+        stats1more = stats1.find('span').text
+        print(caption1 + ': ' + stats1more)
+        index += 1
+        if index == 3:
+            break
+
+    print('\n')
+    running1 = True
+    while running1:
+        end_or_not = input('terminate application? (yes or no)\n')
+        if end_or_not == 'yes':
+            running = False
+            running1 = False
+        elif end_or_not == 'no':
+            print('ok')
+            running1 = False
+        else:
+            print('invalid input, please try again')
