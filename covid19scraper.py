2561ab47ef4f8b8ee85a1d520664b7366255a444
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-08-14 22:23:05 +0800

    search changes
    gave user the option on how they could search for a specific country's covid-19 stats, either by direct search or by selecting from a list

--- covid19scraper.py
+++ covid19scraper.py
@@ -20,46 +20,58 @@ def countrycovidchecker(country):
         index += 1
     daily = covidsoup.find('div', {'class': 'news_date'})
     print(daily.next_sibling.find('strong').text)
-    
-r1 = session.get('https://www.worldometers.info/coronavirus/#countries')
-countrysoup = BeautifulSoup(r1.text, 'html.parser')
-countries = countrysoup.findAll('a', {'class': 'mt_a'})
-countrylist = set()
-for i in countries:
-    countrylist.add(i.text)
 
 running = True
 while running:
-    initial = input('Type the initial letter of country\n').capitalize()
-    index = 1
-    tempcountrylist = []
-    for i in countrylist:
-        if str(i)[0] == initial:
-            print(str(index) + ': ' +i)
-            index += 1
-            if str(i) == 'USA':
-                tempcountrylist.append('US')
-            else:
-                tempcountrylist.append(str(i))
-    ask = input('Choose which (type index or country name)\n')
-    asktype = 'unknown'
-    try:
-        int(ask)
-    except:
-        asktype = 'non-integer'
-    else:
-        asktype = 'integer'
-    if asktype == 'non-integer':
+    firstask = input('1. Direct search\n2. List search\nChoose search option (by typing the index)\n')
+    if firstask == '1':
+        country = input('Type the name of the country\n')
         try:
-            countrycovidchecker(ask)
+            countrycovidchecker(country)
         except:
-            print('Invalid input, try again\n')
-    elif asktype == 'integer':
-        try:
-            countrycovidchecker(tempcountrylist[int(ask)-1])
-        except:
-            print('Invalid input, try again\n')
+            print('Invalid input, try again')
             continue
+
+    elif firstask == '2':
+        r1 = session.get('https://www.worldometers.info/coronavirus/#countries')
+        countrysoup = BeautifulSoup(r1.text, 'html.parser')
+        countries = countrysoup.findAll('a', {'class': 'mt_a'})
+        countrylist = set()
+        for i in countries:
+            countrylist.add(i.text)
+
+        initial = input('Type the initial letter of country\n').capitalize()
+        index = 1
+        tempcountrylist = []
+        for i in countrylist:
+            if str(i)[0] == initial:
+                print(str(index) + ': ' +i)
+                index += 1
+                if str(i) == 'USA':
+                    tempcountrylist.append('US')
+                elif str(i) == 'UAE':
+                    tempcountrylist.append('united-arab-emirates')
+                else:
+                    tempcountrylist.append(str(i))
+        ask = input('Choose which (type index or country name)\n')
+        asktype = 'unknown'
+        try:
+            int(ask)
+        except:
+            asktype = 'non-integer'
+        else:
+            asktype = 'integer'
+        if asktype == 'non-integer':
+            try:
+                countrycovidchecker(ask)
+            except:
+                print('Invalid input, try again\n')
+        elif asktype == 'integer':
+            try:
+                countrycovidchecker(tempcountrylist[int(ask)-1])
+            except:
+                print('Invalid input, try again\n')
+                continue
 
     contloop = True
     while contloop:
