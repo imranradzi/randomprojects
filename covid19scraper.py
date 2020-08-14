a7eec9c375ffb618a7ee7cd954956e303dda9b6e
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-08-14 19:44:29 +0800

    improvements
    added navigation for countries by enabling user to type the initial letter of the country, which then prints out a list of country starting with those initials, allowing user to easily find which country they are looking for

--- covid19scraper.py
+++ covid19scraper.py
@@ -3,20 +3,72 @@ from requests_html import HTMLSession
 
 session = HTMLSession()
 
-country = input('country name goes here\n') + '/'
-r = session.get('https://www.worldometers.info/coronavirus/country/' + country)
-covidsoup = BeautifulSoup(r.text, 'html.parser')
+def countrycovidchecker(country):
+    r = session.get('https://www.worldometers.info/coronavirus/country/' + country)
+    covidsoup = BeautifulSoup(r.text, 'html.parser')
 
-maincontent = covidsoup.findAll('div', {'class': 'maincounter-number'})
-index = 1
-for i in maincontent:
-    if index == 1:
-        print('total covid cases')
-    elif index == 2:
-        print('current total deaths')
-    elif index == 3:
-        print('total recovered')
-    print(i.span.text + '\n')
-    index += 1
-daily = covidsoup.find('div', {'class': 'news_date'})
-print(daily.next_sibling.find('strong').text)
+    maincontent = covidsoup.findAll('div', {'class': 'maincounter-number'})
+    index = 1
+    for i in maincontent:
+        if index == 1:
+            print('total covid cases')
+        elif index == 2:
+            print('current total deaths')
+        elif index == 3:
+            print('total recovered')
+        print(i.span.text + '\n')
+        index += 1
+    daily = covidsoup.find('div', {'class': 'news_date'})
+    print(daily.next_sibling.find('strong').text)
+    
+r1 = session.get('https://www.worldometers.info/coronavirus/#countries')
+countrysoup = BeautifulSoup(r1.text, 'html.parser')
+countries = countrysoup.findAll('a', {'class': 'mt_a'})
+countrylist = set()
+for i in countries:
+    countrylist.add(i.text)
+
+running = True
+while running:
+    initial = input('Type the initial letter of country\n').capitalize()
+    index = 1
+    tempcountrylist = []
+    for i in countrylist:
+        if str(i)[0] == initial:
+            print(str(index) + ': ' +i)
+            index += 1
+            if str(i) == 'USA':
+                tempcountrylist.append('US')
+            else:
+                tempcountrylist.append(str(i))
+    ask = input('Choose which (type index or country name)\n')
+    asktype = 'unknown'
+    try:
+        int(ask)
+    except:
+        asktype = 'non-integer'
+    else:
+        asktype = 'integer'
+    if asktype == 'non-integer':
+        try:
+            countrycovidchecker(ask)
+        except:
+            print('Invalid input, try again\n')
+    elif asktype == 'integer':
+        try:
+            countrycovidchecker(tempcountrylist[int(ask)-1])
+        except:
+            print('Invalid input, try again\n')
+            continue
+
+    contloop = True
+    while contloop:
+        contornot = input('\nContinue? (Y/N)\n')
+        if contornot == 'Y':
+            contloop = False
+            continue
+        elif contornot == 'N':
+            contloop = False
+            running = False
+        else:
+            print('Invalid input, try again\n')
